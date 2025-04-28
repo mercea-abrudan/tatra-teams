214c858c35ebe30bdc19839035e831e25cc81642
mercea.abrudan <mercea.abrudan@gmail.com>
2025-04-28 21:18:01 +0300

    added logger and made adjustments to the main file

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
+import logging
 import os
-from audio_processing import record
+from audio_processing import record_mic_and_out
 from utils import get_next_filename
 
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(module)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
 audio_folder = "../data/recordings"
-file_name = get_next_filename(audio_folder)
-file_path = os.path.join(audio_folder, file_name)
+mic_file_name = get_next_filename(audio_folder, "mic")
+out_file_name = get_next_filename(audio_folder, "out")
+mic_file_path = os.path.join(audio_folder, mic_file_name)
+out_file_path = os.path.join(audio_folder, out_file_name)
 
 
 def main():
-    record(duration=10, sample_rate=44100, output_file_name=file_path)
+    logger.info("Application started.")
+    record_mic_and_out(mic_file_path, out_file_path, 1, 2)
+    logger.info("Application finished.")
 
 
 if __name__ == "__main__":
